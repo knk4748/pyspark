4a7009a06195c858207f180c1bda659994318c80
knk4748 <kchawla_be19@thapar.edu>
2020-12-29 05:02:57 +0530

    Classifiers

--- Classification/OCC.py
+++ Classification/OCC.py
@@ -12,10 +12,6 @@ print("size of the data set: ", dataset.count(), len(dataset.columns))
 dataset.show()
 
 
-def printer(fields):
-    print(type(fields))
-
-# from pyspark.sql.functions import col, split
 from sklearn.preprocessing import LabelEncoder
 data = dataset.select("*").toPandas()
 k = data.keys()
@@ -46,42 +42,24 @@ input_cols = ['cap-shape_n', 'cap-surface_n', 'cap-color_n', 'bruises_n',
 featureassembler=VectorAssembler(inputCols=input_cols,outputCol="features")
 
 XY = featureassembler.transform(df).select("features","label")
-# df.withColumnRenamed("class_n","label").printSchema()
 XY.show()
 
 
 train,test = XY.randomSplit([.8,.2])
 
-# regressor = LinearRegression(featuresCol="Independent Features",labelCol='class_n')
-# model = regressor.fit(train)
-
-# print("COEFFICIENTS  :" ,model.coefficients , "\nintercept  :",model.intercept)
-# predictions_data = model.evaluate(test)
-
-# predictions_data.predictions.show()
 from pyspark.ml.classification import LogisticRegression, OneVsRest
 from pyspark.ml.evaluation import MulticlassClassificationEvaluator
-# load data file.
-# inputData = spark.read.format("libsvm") \
-#     .load("data/mllib/sample_multiclass_classification_data.txt")
-# generate the train/test split.
-# (train, test) = inputData.randomSplit([0.8, 0.2])
-# instantiate the base classifier.
+
 lr = LogisticRegression(maxIter=30, tol=1E-6, fitIntercept=True)
 
-# instantiate the One Vs Rest Classifier.
 ovr = OneVsRest(classifier=lr)
 
-# train the multiclass model.
 ovrModel = ovr.fit(train)
 
-# score the model on test data.
 predictions = ovrModel.transform(test)
 
-# obtain evaluator.
 evaluator = MulticlassClassificationEvaluator(metricName="accuracy")
 
-# compute the classification error on test data.
 accuracy = evaluator.evaluate(predictions)
 print("Test Error = %g" % (1.0 - accuracy))
 
